7fbe3d2dab3000fef93a43da0684e846dc42927f
badees abood <57720463+badees99@users.noreply.github.com>
2025-04-27 15:09:38 +0200

    first commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+
+
+export default function Footer() {
+  return (
+    <footer>
+        <div className="bgGradient"></div>
+ <div>
+        
+<h2>The Brutail marsian Landscape</h2>
+<h1>APOD PROJECT</h1>
+
+<button><i className="fa-solid fa-circle-info"></i></button>
+        </div>
+    </footer>
+   
+  )
+}
---
+++ src/components/SideBar.tsx
@@ -0,0 +1,17 @@
+
+export default function SideBar() {
+  return (
+    <div className="sideBar">
+        <div className="bgOverlay"></div>
+        <div className="sideBarContents">
+        <h2>The Brutal Martain Landascape</h2>
+        <div>
+            <p>Descriptino</p>
+            <p>lk;da dadslk;j asdfkljasd ;lasdjfkl asdkl  sdf 
+                jkdlasfklj asdfklj asdfklj asdfklj asdfklj asdfklj asdfklj
+            </p>
+        </div>
+        </div>
+    </div>
+  )
+}
